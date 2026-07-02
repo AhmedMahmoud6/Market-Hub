e23205c7a4f2f1922aad56a480ef635f4dd2767d
Ahmed Zaki <ahmedmahmoudd626@gmail.com>
2026-07-02 14:49:46 +0300

    add(post.service): added function to the post serivce

--- src/app/core/services/post.service.ts
+++ src/app/core/services/post.service.ts
@@ -1,6 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { PostsResponse } from '../models/post.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class PostService {}
+export class PostService {
+  private http = inject(HttpClient);
+  private apiUrl = environment.apiUrl;
+
+  getUserPostsById(userId: number): Observable<PostsResponse> {
+    return this.http.get<PostsResponse>(`${this.apiUrl}/posts/user/${userId}`);
+  }
+}
